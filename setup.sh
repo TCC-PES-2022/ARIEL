40e23bde6a82ab85f91eabb4c9ab69ba45058619
Kollins <kgl2@cin.ufpe.br>
2022-10-24 09:28:07 -0300

    Install Qt in setup.sh

--- setup.sh
+++ setup.sh
@@ -5,9 +5,16 @@ INSTALL_PATH=$HOME/pes
 git submodule update --init --recursive --remote
 
 sudo apt update
-sudo apt install -y --allow-downgrades build-essential=12.9ubuntu3 libcjson-dev=1.7.15-1 libgcrypt20-dev=1.9.4-3ubuntu3 openssl=3.0.2-0ubuntu1 libtinyxml2-dev=9.0.0+dfsg-3
+
+# On Ubuntu, all packages are from jammy repository
+
+sudo apt install -y --allow-downgrades build-essential=12.9ubuntu3 libcjson-dev=1.7.15-1 libgcrypt20-dev=1.9.4-3ubuntu3 openssl=3.0.2-0ubuntu1 libtinyxml2-dev=9.0.0+dfsg-3 
 
 sudo apt install -y libgtest-dev=1.11.0-3 cmake-data=3.22.1-1ubuntu1 cmake=3.22.1-1ubuntu1 lcov=1.15-1
+
+# Install Qt
+
+sudo apt install -y --allow-downgrades libqt5concurrent5=5.15.3+dfsg-2 libqt5core5a=5.15.3+dfsg-2 libqt5dbus5=5.15.3+dfsg-2 libqt5gui5=5.15.3+dfsg-2 libqt5network5=5.15.3+dfsg-2 libqt5printsupport5=5.15.3+dfsg-2 libqt5sql5=5.15.3+dfsg-2 libqt5test5=5.15.3+dfsg-2 libqt5widgets5=5.15.3+dfsg-2 libqt5xml5=5.15.3+dfsg-2 qt5-qmake-bin=5.15.3+dfsg-2 qt5-qmake=5.15.3+dfsg-2 qtbase5-dev-tools=5.15.3+dfsg-2 qtbase5-dev=5.15.3+dfsg-2
 
 # Build LogManager
 cd modules/LogManager
